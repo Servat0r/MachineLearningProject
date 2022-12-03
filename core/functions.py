1. Added SparseCrossEntropy as metric for classification; 2. Fixed Softmax; 3. Added InteractiveLogger for "stdout-logging"; 4. Added a couple of transforms (OneHotEncoder, StandardScaler); 5. Split scikit-learn datasets tests in separate files for classification and regression and added some more tests.

core/functions.py
@@ -33,13 +33,13 @@ class Softmax(Callable):
         # Reshape for summing elements on the ("last two") diagonals
         l, n = y.shape[0], y.shape[2]
         ind = np.array(range(n))
-        y[:, ind, ind] = s[:, 0, ind]
+        y[:, ind, ind] += s[:, 0, ind]
         return y
 
-    def vjp(self, x: np.ndarray, dvals: np.ndarray):
+    def vjp(self, x: np.ndarray, dvals: np.ndarray):    # todo check!
         s = self(x)
         sd = s * dvals
-        sd_sum = np.sum(sd, axis=1, keepdims=True)  # todo check axis!
+        sd_sum = np.sum(sd, axis=-1, keepdims=True)
         return sd - sd_sum * s
 
 
@@ -63,17 +63,20 @@ class CategoricalCrossEntropy(Callable):
             correct_confidences = x_clipped[range(samples), 0, truth]
         elif len(trshape) == 3:  # (l, 1, n)
             filtered_x = x_clipped * truth
-            correct_confidences = np.sum(filtered_x, axis=2)
+            correct_confidences = np.sum(filtered_x, axis=-1)
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
 
     def grad(self, x: np.ndarray, truth: np.ndarray):
-        x_clip = - 1.0 / np.clip(x, self.clip_value, 1 - self.clip_value)   # todo maybe this clip should not occur
+        # x_clip = - 1.0 / np.clip(x, self.clip_value, 1 - self.clip_value)   # todo maybe this clip should not occur
+        x_clip = - 1.0 / x
         trshape = truth.shape
         if len(trshape) == 1:
             # If labels are sparse, turn them into one-hot encoded ones
-            truth = np.eye(x.shape[2])[truth]
-            truth = np.reshape(truth, (trshape[0], 1, x.shape[2]))
+            ohe_truth = np.zeros_like(x_clip)  # (l, 1, n)
+            for i in range(trshape[0]):
+                ohe_truth[i, 0, truth[i]] = 1.
+            truth = ohe_truth
         result = x_clip * truth
         return result
 
@@ -112,6 +115,14 @@ def categorical_accuracy(pred: np.ndarray, truth: np.ndarray, dtype=np.int32) ->
     pred_indexes = np.argmax(pred, axis=-1)
     truth_indexes = np.argmax(truth, axis=-1)
     return np.equal(pred_indexes, truth_indexes).astype(dtype)
+
+
+def sparse_categorical_accuracy(pred: np.ndarray, truth: np.ndarray, dtype=np.int32) -> np.ndarray:
+    """
+    Accuracy for integer labels.
+    """
+    pred_indexes = np.argmax(pred, axis=-1).astype(np.int).reshape(truth.shape)
+    return np.equal(pred_indexes, truth).astype(dtype)
 
 
 def binary_accuracy(pred: np.ndarray, truth: np.ndarray, threshold=0.5, dtype=np.int32):
@@ -155,6 +166,7 @@ __all__ = [
     'SquaredError',
     'accuracy',
     'categorical_accuracy',
+    'sparse_categorical_accuracy',
     'binary_accuracy',
     'mean_euclidean_error',
     'root_mean_squared_error',

tests/keras_tests/scikit_learn_datasets_regression.py
@@ -1,12 +1,10 @@
-# Comparison tests on scikit-learn datasets with tests/scikit_learn_datasets.py
+# Comparison tests on scikit-learn datasets with tests/scikit_learn_datasets_regression.py
 from __future__ import annotations
-from sklearn.datasets import load_diabetes, fetch_california_housing                # Regression datasets
-# from sklearn.datasets import load_iris, load_wine, load_files, load_breast_cancer   # Classification datasets
+from sklearn.datasets import load_diabetes, fetch_california_housing  # Regression datasets
 from sklearn.model_selection import train_test_split
 
 from tests.utils import *
 import tensorflow as tf
-# layers = tf.keras.layers
 
 
 def keras_test_diabetes(hidden_sizes=(4,), winit_low=-0.1, winit_high=0.1, epoch_shuffle=True):
@@ -69,7 +67,8 @@ def keras_test_california_housing(hidden_sizes=(4,), winit_low=-0.1, winit_high=
 
     model = tf.keras.Sequential()
     initializer = tf.keras.initializers.RandomUniform(winit_low, winit_high, seed=0)
-    l2_regularizer = tf.keras.regularizers.L2(1e-7)
+    # l2_regularizer = tf.keras.regularizers.L2(1e-7)
+    l2_regularizer = None
     for hsize in hidden_sizes:
         model.add(
             tf.keras.layers.Dense(
@@ -92,6 +91,6 @@ def keras_test_california_housing(hidden_sizes=(4,), winit_low=-0.1, winit_high=
 
 
 if __name__ == '__main__':
-    # keras_test_diabetes(hidden_sizes=(4,))
-    keras_test_california_housing(hidden_sizes=(4,))
+    keras_test_diabetes(hidden_sizes=(4,))
+    # keras_test_california_housing(hidden_sizes=(4,))
     exit(0)